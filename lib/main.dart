77185c5f937a4e5a26221c46264327281b344261
Takuya OHASHI <tky.ohashi@gmail.com>
2019-04-06 10:41:23 +0900

    Add Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flat_flutter/flat_list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: FlatList(),
     );
   }
 }
