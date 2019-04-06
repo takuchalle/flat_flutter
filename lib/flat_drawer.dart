441bd3cb239eb45fa9c2d3b8eaab5864873fa75f
Takuya OHASHI <tky.ohashi@gmail.com>
2019-04-06 11:59:06 +0900

    Add DrawerHeader

--- lib/flat_drawer.dart
+++ lib/flat_drawer.dart
@@ -6,6 +6,8 @@ class FlatDrawer extends StatefulWidget {
 }
 
 class _FlatDrawerState extends State<FlatDrawer> {
+  bool showUserAccount = true;
+
   Widget _buildUserAccountsDrawerHeader() {
     return UserAccountsDrawerHeader(
       accountName: Text("User Name"),
@@ -17,12 +19,31 @@ class _FlatDrawerState extends State<FlatDrawer> {
     );
   }
 
+  Widget _buildDrawerHeader() {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        color: const Color(0xff7c94b6),
+      ),
+    );
+  }
+
+  void _onChanged(bool value) => setState(() => showUserAccount = value);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: <Widget>[
-          _buildUserAccountsDrawerHeader(),
+          showUserAccount
+              ? _buildUserAccountsDrawerHeader()
+              : _buildDrawerHeader(),
+          SwitchListTile(
+            value: showUserAccount,
+            onChanged: _onChanged,
+            title: new Text(
+              'Switch Header',
+            ),
+          )
         ],
       ),
     );
